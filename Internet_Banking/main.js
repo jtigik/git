d89cdc1b4b122de2eb151ec4bffe3f72afcef09d
Jeferson Tigik <prof.tigik@gmail.com>
2023-05-10 00:54:12 -0300

    Updating .html, .css, .js

--- Internet_Banking/main.js
+++ Internet_Banking/main.js
@@ -1,35 +1,66 @@
+const form = document.getElementById('form_dep');
+const nomeBeneficiario = document.getElementById('nome_beneficiario');
+let formValido = false;
+
 function validaNome(nomeCompleto){
     const nomeComoArray = nomeCompleto.split(' ');
 
     return nomeComoArray.length >= 2;
 }
 
-const form = document.getElementById('form_dep');
-
 form.addEventListener('submit', function(e){
 
-    let formValido = false;
+    //let formValido = false;
+    //Elevando a variável 'formValido' à global
 
     e.preventDefault(); //Cancela a ação padrão que limpa o formulário.
 
-    const nomeBeneficiario = document.getElementById('nome_beneficiario');
+    //Elevando o campo nome_beneficiario à global
     const numeroContaBeneficiario = document.getElementById('numero_conta');
     const valorDeposito = document.getElementById('valor_deposito');
 
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta ${numeroContaBeneficiario.value}`
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta <b>${numeroContaBeneficiario.value}</b>`
 
     formValido = validaNome(nomeBeneficiario.value);
 
     if(formValido){
-        alert(mensagemSucesso);
+        // alert(mensagemSucesso);
+        //Escrevendo a mensagem de sucesso no HTML;
+        //Construir primeiro: document.querySelector('.success_message').innerHTML = mensagemSucesso;
+        //Editando elementos CSS com o JS
+
+        const containerMensagemSucesso = document.querySelector('.success_message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
 
     }else{
-        alert("O nome não está completo!");
+        // alert("O nome não está completo!");
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error_message').style.display = 'block';
     }
 });
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    //console.log(e); - Exibindo todos os atributos do elemento.
+    console.log(e.target.value);
+
+    //A cada digitação do usuário, verifica se o nome é válido.
+
+    formValido = validaNome(e.target.value);
+
+    if(!formValido){
+        //nomeBeneficiario.style.border = '1px solid red';
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error_message').style.display = 'block';
+    }else{
+        //nomeBeneficiario.style = '';
+        nomeBeneficiario.classList.remove('error');
+        //Removendo a Class Style de erro
+        document.querySelector('.error_message').style.display = 'none';
+    }
+})
